28c75df05d9220c69ae29ff4e0052a69a9087f6d
Ramixzx <ramiro.mijalovich@gmail.com>
2022-03-08 15:48:20 -0300

    Removed test code (extracted from sort module) from exercise 1

--- src/exercise1/index.js
+++ src/exercise1/index.js
@@ -7,50 +7,5 @@ const countdown = (n) => {
     countdown(n - 1)
 }
 
+//Example 
 countdown(17);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const sortArrayOfStrings = (array) => {
-    
-    let newArray = array.map((element, i) => {
-        return { index: i, value: element.toLowerCase()}
-    })
-    
-
-    newArray.sort((a, b) => {
-        if(a.value > b.value){
-            return 1
-        }
-        if(a.value < b.value){
-            return -1
-        }
-        return 0
-    })
-    
-
-    let theArray = newArray.map(element => {
-        return array[element.index]
-    })
-    
-    console.log(theArray)
-}
